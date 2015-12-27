b2f182ad97bc5b4a4806c50a35a82a7dc9380609
Coma <Coma@powerbot.org>
2015-12-27 15:28:48 -0500

    updated HelloRSBot to reflect guidelines

--- src/coma/HelloRSBot.java
+++ src/coma/HelloRSBot.java
@@ -1,8 +1,13 @@
+package coma;
+
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.Script;
 import org.powerbot.script.rt6.ClientContext;
 
-@Script.Manifest(name = "Hello, RSBot!", description = "A 'Hello, World' example for RSBot")
+@Script.Manifest(
+        name = "Hello, RSBot!", properties = "author=Coma; topic=1296203; client=6;",
+        description = "A 'Hello, World' example for RSBot"
+)
 public class HelloRSBot extends PollingScript<ClientContext> {
     @Override
     public void poll() {
