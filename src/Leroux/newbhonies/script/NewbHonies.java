3923a729521b0a59cf6173f9997eec4f1cfdb625
ValintineLeroux <hudson.codya@yahoo.com>
2015-12-28 06:01:46 -0800

    init push for NewbHonies

---
+++ src/Leroux/newbhonies/script/NewbHonies.java
@@ -0,0 +1,107 @@
+package Leroux.newbhonies.script;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import Leroux.newbhonies.tasks.Deposit;
+import Leroux.newbhonies.tasks.GrabHonies;
+import Leroux.newbhonies.tasks.Move_ToBank;
+import Leroux.newbhonies.tasks.Move_ToField;
+import Leroux.newbhonies.constants.InvItems;
+
+import org.powerbot.script.MessageEvent;
+import org.powerbot.script.MessageListener;
+import org.powerbot.script.PaintListener;
+import org.powerbot.script.PollingScript;
+import org.powerbot.script.Script;
+import org.powerbot.script.rt6.ClientContext;
+import org.powerbot.script.rt6.GeItem;
+
+@Script.Manifest(name = "Newb Honies", description = "Collects HoneyComb in Seers Village.", properties = "client=6;topic=1265445;author=Leroux;")
+
+public class NewbHonies extends PollingScript<ClientContext> implements PaintListener, MessageListener {
+	
+	private List<Task> taskList = new ArrayList<Task>();
+	private InvItems items = new InvItems(ctx);
+	
+	private final Color mouseColor = new Color(82, 181, 209);
+	private final Color boxColor = new Color(82, 181, 209, 125);
+
+	private final Color textColor = new Color(0,0,0);    
+	private final Font font = new Font("Arial",Font.BOLD, 15);
+	
+	private long initialTime;
+	private int profit, honeyPrice, honiesGrabbed, hours, minutes, seconds;
+	private double runTime, honiesPerHour, profitPerHour;
+
+	@Override
+	public void start() {
+		if(!ctx.backpack.select().contains(items.getRepellant())){
+			System.out.print(" Insect repellant is needed! ");
+			ctx.controller.stop();
+		} else {
+			taskList.addAll(Arrays.asList(new Deposit(ctx), new Move_ToField(ctx), new GrabHonies(ctx), new Move_ToBank(ctx)));
+			System.out.print(" Tasks Added ");
+		}
+		
+		honeyPrice = GeItem.price(items.getHoney());
+		initialTime = System.currentTimeMillis();
+		System.out.print(" " + honeyPrice);
+		
+	}
+	
+	 @Override
+	    public void poll() {    
+	    	for (Task task : taskList) {
+	    		if(task.activate()) {
+	            	task.execute();
+	            	System.out.print(" " + task + " ");
+	    		}
+	    	}        
+	    }
+	
+	@Override
+	public void messaged(MessageEvent m) {
+		if(m.text().startsWith("You take")) {
+    		honiesGrabbed++;
+    		profit += honeyPrice;
+    	} 
+	}
+
+	@Override
+	public void repaint(Graphics g1) {
+		
+    	Graphics2D g = (Graphics2D)g1;
+    	int x = (int) ctx.input.getLocation().getX();
+    	int y = (int) ctx.input.getLocation().getY();		
+			
+    	g.setColor(mouseColor);
+    	g.drawLine(x, y - 10, x, y + 10);
+    	g.drawLine(x - 10, y, x + 10, y);
+    	
+    	hours = (int) ((System.currentTimeMillis() - initialTime) / 3600000);
+		minutes = (int) ((System.currentTimeMillis() - initialTime) / 60000 % 60);
+		seconds = (int) ((System.currentTimeMillis() - initialTime) / 1000) % 60;	
+		runTime = (double) (System.currentTimeMillis() - initialTime) / 3600000;								
+		honiesPerHour = honiesGrabbed / runTime;		
+		profitPerHour = profit / runTime;
+	    
+		g.setColor(boxColor);
+		g.fillRoundRect(8, 346, 504, 126, 5,  5);             
+		g.setColor(textColor);
+		g.setFont(font);
+		
+		g.drawString("Newb Honies v1.0", 180, 364);            
+		g.drawString("Grabbed: " + honiesGrabbed + "(" + (int)honiesPerHour + "/hour)", 22, 385);             
+		g.drawString("Profit: " + profit + "(" + (int)profitPerHour + "/hour)", 22, 400);           
+		g.drawString("Time Running: " + hours +":" + minutes + ":" + seconds, 22, 415);           
+
+    	
+	}
+	
+}
