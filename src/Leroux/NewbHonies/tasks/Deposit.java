9ed185e9655cf7eec616de8b6623521f37db6632
ValintineLeroux <hudson.codya@yahoo.com>
2015-12-28 16:31:17 -0800

    Fixed
    May now compile

---
+++ src/Leroux/NewbHonies/tasks/Deposit.java
@@ -0,0 +1,73 @@
+package Leroux.NewbHonies.tasks;
+
+import Leroux.NewbHonies.constants.Areas;
+import Leroux.NewbHonies.constants.InvItems;
+import Leroux.NewbHonies.script.Task;
+import org.powerbot.script.Condition;
+import org.powerbot.script.rt6.ClientContext;
+import org.powerbot.script.rt6.GameObject;
+import org.powerbot.script.rt6.Interactive;
+import org.powerbot.script.rt6.Item;
+
+import java.util.concurrent.Callable;
+
+public class Deposit extends Task {
+
+	public Deposit(ClientContext ctx) {
+		super(ctx);
+	}
+	
+	private Areas areas = new Areas();
+	private InvItems items = new InvItems(ctx);
+	
+	private final int[] bankBounds = {-256, 256, -512, 0, -256, 256};
+	
+	@Override
+	public boolean activate() {
+		
+		return !ctx.objects.select().id(25808).each(Interactive.doSetBounds(bankBounds)).isEmpty()
+					&& areas.getBankArea().contains(ctx.players.local())
+						&& ctx.backpack.select().contains(items.getHoneyComb());
+		
+	}
+
+	@Override
+	public void execute() {
+
+		GameObject bank = ctx.objects.nearest().poll();
+		Item honeyComb = ctx.backpack.select().id(items.getHoneyComb().id()).poll();
+		
+		if(!bank.inViewport()) {
+			ctx.movement.step(bank);
+			ctx.camera.turnTo(bank);
+		}
+		
+		if(!ctx.backpack.itemSelected() && !ctx.widgets.widget(1188).component(0).visible()) {
+			
+			if(honeyComb.interact("Use")) {
+				
+				bank.interact("Use");
+				
+				Condition.wait(new Callable<Boolean>() {
+					@Override
+					public Boolean call() throws Exception {
+						return ctx.widgets.widget(1188).component(0).visible();
+					}
+				});	
+					
+				}
+				
+			}
+		
+	
+		if(ctx.widgets.widget(1188).component(6).visible()) {
+		
+			ctx.widgets.widget(1188).component(6).interact("Continue");
+		
+		}
+		
+		
+		
+	}
+
+}
---
+++ src/Leroux/NewbHonies/tasks/Move_ToBank.java
@@ -0,0 +1,33 @@
+package Leroux.NewbHonies.tasks;
+
+import Leroux.NewbHonies.constants.Areas;
+import Leroux.NewbHonies.constants.Paths;
+import Leroux.NewbHonies.methods.Walker;
+import Leroux.NewbHonies.script.Task;
+import org.powerbot.script.rt6.ClientContext;
+
+public class Move_ToBank extends Task {
+
+	public Move_ToBank(ClientContext ctx) {
+		super(ctx);
+	}
+	
+	private Walker walk = new Walker(ctx);
+	private Paths path = new Paths();
+	private Areas area = new Areas();
+
+	@Override
+	public boolean activate() {
+		
+		return !area.getBankArea().contains(ctx.players.local())
+					&& ctx.backpack.select().count() == 28;
+		
+	}
+
+	@Override
+	public void execute() {
+		
+		walk.followPath(path.getToBank(), -2, 2, area.getBankTile());
+
+	}
+}
