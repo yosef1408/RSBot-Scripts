5c3977f3f2cf860154911cd0c369ea88fe558070
Tallah <tallah.khan@live.co.uk>
2017-06-11 22:29:48 +0100

    gui, cooking on spot

--- src/ryukis215/scripts/RyuksMultiTaskingFisher/Actions.java
+++ src/ryukis215/scripts/RyuksMultiTaskingFisher/Actions.java
@@ -1,9 +1,12 @@
 package ryukis215;
 
+import java.util.concurrent.Callable;
+
 import org.powerbot.script.Condition;
 import org.powerbot.script.Filter;
 import org.powerbot.script.Random;
 import org.powerbot.script.Tile;
+import org.powerbot.script.rt4.Component;
 import org.powerbot.script.rt4.GameObject;
 import org.powerbot.script.rt4.Interactive;
 import org.powerbot.script.rt4.Item;
@@ -17,8 +20,9 @@ import org.powerbot.script.rt4.TilePath;
  */
 public class Actions extends Controller {
 		
-	int[] fishList = new int[]{335, 331, 317, 321, 359, 377};
-	static int[] fishingAnimationList = new int[]{621, 622, 623};
+	int[] fishList = new int[]{335, 331, 317, 321, 359, 377, 329, 343, 333};
+	static int[] fishingAnimationList = new int[]{621, 622, 623, 619};
+	long lastChecked;
 	
 	/**
 	 * when provided a npc and and an action, will 
@@ -55,8 +59,11 @@ public class Actions extends Controller {
 			for(int id: fishList){
 				if(item.id() == id){
 					if(!item.valid()){
-						ctx.widgets.widget(548).component(50).interact("Inventory");
+						ctx.widgets.widget(548).component(50).click();
 						Condition.sleep(Random.nextInt(83, 116));
+					}
+					if(ctx.inventory.selectedItemIndex() != -1){
+						ctx.inventory.itemAt(ctx.inventory.selectedItemIndex()).click();
 					}
 					if (shiftClickOn) {
 					    item.click(true);
@@ -67,6 +74,40 @@ public class Actions extends Controller {
 			}
 		}
 		if(shiftClickOn)ctx.input.send("{VK_SHIFT up}");
+	}
+	
+	public void cookFishes(){
+		if (ctx.players.local().animation() != -1) {
+			lastChecked = System.currentTimeMillis();
+		}
+
+		if (System.currentTimeMillis() - lastChecked > Random.nextInt(1000, 1700)) {
+			final GameObject fire = ctx.objects.select().id(26185).nearest().poll();
+			if(!fire.inViewport())
+				gotoTile(fire.tile());
+			
+			final Item raw = ctx.inventory.select().id(335, 331).poll();	
+			final Component cookingInterface = ctx.widgets.component(307, 5);
+			
+			if (!cookingInterface.visible() && raw.interact("Use", raw.name())
+					&& fire.interact("Use", fire.name())) {
+				Condition.wait(new Callable<Boolean>() {
+					@Override
+					public Boolean call() throws Exception {
+						return cookingInterface.visible();
+					}
+				}, 250, 10);
+			} else if (cookingInterface.visible()) {
+				cookingInterface.interact("Cook All");
+				Condition.wait(new Callable<Boolean>() {
+					@Override
+					public Boolean call() throws Exception {
+						return !cookingInterface.visible()
+								&& ctx.players.local().animation() != -1;
+					}
+				}, 250, 10);
+			}
+		}
 	}
 	
 	public void dropItem(Item item){
@@ -104,6 +145,7 @@ public class Actions extends Controller {
 		TilePath path = ctx.movement.newTilePath(travelPath);
 		path.traverse();
 	}
+	
 	
 	/**
 	 * will see if the gate is open in the chicken farm
@@ -163,38 +205,6 @@ public class Actions extends Controller {
 		return false;
 	}
 	
-	public final static Tile[] travelPath_old = { new Tile(3237, 3296, 0),
-		new Tile(3238, 3299, 0), new Tile(3238, 3302, 0),
-		new Tile(3236, 3305, 0), new Tile(3233, 3307, 0),
-		new Tile(3230, 3307, 0), new Tile(3226, 3311, 0),
-		new Tile(3224, 3314, 0), new Tile(3223, 3317, 0),
-		new Tile(3221, 3320, 0), new Tile(3218, 3321, 0),
-		new Tile(3217, 3324, 0), new Tile(3216, 3327, 0),
-		new Tile(3215, 3330, 0), new Tile(3213, 3333, 0),
-		new Tile(3210, 3336, 0), new Tile(3207, 3339, 0),
-		new Tile(3206, 3342, 0), new Tile(3206, 3345, 0),
-		new Tile(3206, 3349, 0), new Tile(3205, 3352, 0),
-		new Tile(3202, 3356, 0), new Tile(3200, 3359, 0),
-		new Tile(3197, 3361, 0), new Tile(3194, 3363, 0),
-		new Tile(3191, 3364, 0), new Tile(3191, 3367, 0),
-		new Tile(3191, 3370, 0), new Tile(3189, 3373, 0),
-		new Tile(3187, 3376, 0), new Tile(3184, 3378, 0),
-		new Tile(3181, 3380, 0), new Tile(3178, 3380, 0),
-		new Tile(3175, 3381, 0), new Tile(3171, 3385, 0),
-		new Tile(3167, 3386, 0), new Tile(3166, 3389, 0),
-		new Tile(3163, 3389, 0), new Tile(3160, 3391, 0),
-		new Tile(3157, 3394, 0), new Tile(3154, 3394, 0),
-		new Tile(3151, 3394, 0), new Tile(3148, 3394, 0),
-		new Tile(3145, 3393, 0), new Tile(3141, 3393, 0),
-		new Tile(3138, 3395, 0), new Tile(3134, 3399, 0),
-		new Tile(3130, 3402, 0), new Tile(3127, 3404, 0),
-		new Tile(3124, 3404, 0), new Tile(3122, 3408, 0),
-		new Tile(3120, 3411, 0), new Tile(3117, 3412, 0),
-		new Tile(3114, 3415, 0), new Tile(3113, 3418, 0),
-		new Tile(3110, 3419, 0), new Tile(3107, 3420, 0),
-		new Tile(3104, 3420, 0), new Tile(3101, 3420, 0),
-		new Tile(3099, 3423, 0), new Tile(3101, 3426, 0) };
-
 public static final Tile[] travelPath = { new Tile(3238, 3297, 0),
 		new Tile(3238, 3299, 0), new Tile(3238, 3301, 0),
 		new Tile(3237, 3303, 0), new Tile(3236, 3305, 0),
@@ -244,7 +254,17 @@ public static final Tile[] travelPath = { new Tile(3238, 3297, 0),
 		new Tile(3104, 3420, 0), new Tile(3102, 3420, 0),
 		new Tile(3100, 3421, 0), new Tile(3099, 3423, 0),
 		new Tile(3099, 3425, 0), new Tile(3101, 3426, 0) };
-	
 
+	public static final Tile[] catherbyPath = { new Tile(2845, 3430, 0),
+			new Tile(2843, 3432, 0), new Tile(2841, 3433, 0),
+			new Tile(2839, 3435, 0), new Tile(2837, 3435, 0),
+			new Tile(2834, 3435, 0), new Tile(2832, 3436, 0),
+			new Tile(2830, 3436, 0), new Tile(2828, 3436, 0),
+			new Tile(2826, 3436, 0), new Tile(2824, 3436, 0),
+			new Tile(2822, 3436, 0), new Tile(2820, 3436, 0),
+			new Tile(2818, 3436, 0), new Tile(2816, 3436, 0),
+			new Tile(2814, 3436, 0), new Tile(2812, 3436, 0),
+			new Tile(2810, 3436, 0), new Tile(2809, 3438, 0),
+			new Tile(2809, 3440, 0) };
 
 }
