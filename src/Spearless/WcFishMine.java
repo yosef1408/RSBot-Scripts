65d79d33f2a3f5fb5590db770a93b7a9d22b3364
TendingInfinite <gaston.ezequiel.ortega@gmail.com>
2017-06-16 12:40:32 -0300

    Update WcFishMine.java

--- src/Spearless/WcFishMine.java
+++ src/Spearless/WcFishMine.java
@@ -366,7 +366,7 @@ public class WcFishMine extends PollingScript<ClientContext>implements PaintList
         g.drawLine(x-10,y,x+10,y);
 
         hours=(int)((System.currentTimeMillis()-initialTime)/3600000);
-        minutes=(int)((System.currentTimeMillis()-initialTime)/60000)+117;
+        minutes=(int)((System.currentTimeMillis()-initialTime)/60000);
         seconds=(int)((System.currentTimeMillis()-initialTime)/1000)%60;
         runTime= (double)(System.currentTimeMillis()-initialTime)/3600000;
 
