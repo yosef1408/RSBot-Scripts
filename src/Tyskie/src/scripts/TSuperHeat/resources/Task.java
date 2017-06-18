79abda3c6a327df078e0de874207a6fd61c34614
tyskie <thijs.thijssen1@hotmail.com>
2017-06-18 20:39:22 +0200

    Added Tyskie's TSuperHeat

---
+++ src/Tyskie/src/scripts/TSuperHeat/resources/Task.java
@@ -0,0 +1,17 @@
+package scripts.TSuperHeat.resources;
+
+import org.powerbot.script.rt4.ClientAccessor;
+import org.powerbot.script.rt4.ClientContext;
+
+/**
+ * Created by Tyskie on 17-6-2017.
+ */
+public abstract class Task extends ClientAccessor {
+
+    public Task(ClientContext ctx) {
+        super(ctx);
+    }
+
+    public abstract boolean activate();
+    public abstract void execute();
+}
