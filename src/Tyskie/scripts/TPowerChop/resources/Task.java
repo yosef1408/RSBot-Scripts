cc3134af691a89643311343a7ab656a649c54d34
tyskie <thijs.thijssen1@hotmail.com>
2017-06-29 18:00:06 +0200

    Added Tyskie's TPowerChop

---
+++ src/Tyskie/scripts/TPowerChop/resources/Task.java
@@ -0,0 +1,17 @@
+package Tyskie.scripts.TPowerChop.resources;
+
+import org.powerbot.script.rt4.ClientAccessor;
+import org.powerbot.script.rt4.ClientContext;
+
+/**
+ * Created by Tyskie on 20-6-2017.
+ */
+public abstract class Task extends ClientAccessor {
+
+    public Task(ClientContext ctx) {
+        super(ctx);
+    }
+
+    public abstract boolean activate();
+    public abstract void execute();
+}
