0bf0e0a089a5fec3739252261cf6d47670af52c4
noobienoobie123 <noobie2021@gmail.com>
2017-07-17 10:32:10 -0400

    Added Files
    Changed package name

---
+++ src/noobienoobie123/Task.java
@@ -0,0 +1,18 @@
+package noobienoobie123;
+
+import org.powerbot.script.rt4.ClientAccessor;
+import org.powerbot.script.rt4.ClientContext;
+
+/**
+ * Created by larry on 7/12/2017.
+ */
+public abstract class Task extends ClientAccessor{
+
+    public Task(ClientContext ctx) {
+        super(ctx);
+    }
+
+    public abstract boolean activate();
+    public abstract void execute();
+
+}
