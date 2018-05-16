da50440ddd0a85803becee45fe3fa6daf0aa0fbe
SCP-914 <39167992+SCP-914@users.noreply.github.com>
2018-05-16 14:49:46 -0400

    fixed widget ids
    forgot to add an import statement

--- src/Elt/AlKharidFishNCook/tasks/Cook.java
+++ src/Elt/AlKharidFishNCook/tasks/Cook.java
@@ -4,6 +4,7 @@ import org.powerbot.script.Condition;
 import org.powerbot.script.Tile;
 import org.powerbot.script.rt4.ClientContext;
 import org.powerbot.script.rt4.GameObject;
+import org.powerbot.script.rt4.Component;
 import Elt.AlKharidFishNCook.Task;
 
 import java.util.concurrent.Callable;
@@ -12,6 +13,7 @@ public class Cook extends Task {
 
     private final int rangeId = 26181;
     private final int cookingAnimationId = 896;
+    private final Component cookingWidget=ctx.widgets.widget(270).component(0);
     private final Tile[] spotToRangePath = {new Tile(3273, 3145, 0), new Tile(3273, 3149, 0), new Tile(3273, 3153, 0), new Tile(3273, 3157, 0), new Tile(3273, 3161, 0), new Tile(3273, 3166, 0), new Tile(3274, 3170, 0), new Tile(3275, 3174, 0), new Tile(3276, 3178, 0), new Tile(3272, 3180, 0)};
 
     public Cook(ClientContext ctx) {
@@ -35,25 +37,26 @@ public class Cook extends Task {
     @Override
     public void execute() {
         if (!ctx.objects.select().id(rangeId).isEmpty() && ctx.objects.select().id(rangeId).nearest().poll().tile().distanceTo(ctx.players.local()) < 3) {
-            if (ctx.widgets.component(307, 3).visible()) {
-                ctx.widgets.widget(307).component(3).interact("Cook All");
-                Condition.wait(new Callable<Boolean>() {
-                    @Override
-                    public Boolean call() throws Exception {
-                        return ctx.players.local().animation() == cookingAnimationId;
-                    }
-                }, 150, 20);
-            } else {
-                final GameObject range = ctx.objects.select().id(rangeId).nearest().poll();
-                clickRawFish();
-                range.click();
-                Condition.wait(new Callable<Boolean>() {
-                    @Override
-                    public Boolean call() throws Exception {
-                        return ctx.widgets.component(307, 3).visible();
-                    }
-                }, 150, 20);
+            final GameObject range = ctx.objects.select().id(rangeId).nearest().poll();
+            clickRawFish();
+            range.click();
+
+            Condition.wait(new Callable<Boolean>() {
+                @Override
+                public Boolean call() throws Exception {
+                    return cookingWidget.visible();
+                }
+            });
+            final Component cookMenuMain=ctx.widgets.widget(270).component(14).component(38);
+            if(cookMenuMain.valid()){
+                System.out.println("COOK "+cookMenuMain.click());
             }
+            Condition.wait(new Callable<Boolean>() {
+                @Override
+                public Boolean call() throws Exception {
+                    return ctx.players.local().animation() != cookingAnimationId;
+                }
+            });
         } else {
             ctx.movement.newTilePath(spotToRangePath).traverse();
         }
